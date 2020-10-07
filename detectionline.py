2f5e7df6f7ba2e7a8536c74ceb8051051bf270b0
Shikha verma <39978663+1602085@users.noreply.github.com>
2020-10-07 20:40:34 +0530

    Update detectionline.py

--- detectionline.py
+++ detectionline.py
@@ -1,7 +1,7 @@
 import cv2                
 import numpy as np
 
-img=cv2.imread("/home/shikha/Documents/road1.jpeg")  # read rgb image
+img=cv2.imread("road.jpeg")  # read rgb image
 gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    # convert bgr image to gray
 
 
